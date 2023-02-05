eb5fdb1ce46b34089751bfff2b735cc4104bb220
Youssef Attai <youssefgalalnazem@gmail.com>
2023-02-05 06:00:25 +0200

    Include the authentication router

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from router.auth import router as auth_router
 app = FastAPI()
 
 app.add_middleware(
@@ -10,6 +11,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.include_router(auth_router)
 
 @app.get('/')
 async def main():
