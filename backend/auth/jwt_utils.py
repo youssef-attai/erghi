5e9042ad783eb3e99da0e33148015f610573d7c8
Youssef Attai <youssefgalalnazem@gmail.com>
2023-02-05 21:41:43 +0200

    Get current user object instead of ID only

--- backend/auth/jwt_utils.py
+++ backend/auth/jwt_utils.py
@@ -1,6 +1,11 @@
 from datetime import datetime, timedelta
+from bson import ObjectId
 from jose import JWTError, jwt
-from fastapi import HTTPException, status
+from fastapi import Depends, HTTPException, status
+from pymongo.collection import Collection
+from auth.oauth2_scheme import oauth2_scheme
+from database.collections import get_users_collection
+from schema.user import CurrentUser
 
 SECRET_KEY = "potato"
 ALGORITHM = "HS256"
@@ -15,7 +20,10 @@ def create_access_token(data: dict, expires_delta: timedelta):
     return encoded_jwt
 
 
-def get_user_id(token: str) -> str:
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    users_collection: Collection = Depends(get_users_collection)
+) -> CurrentUser:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -27,7 +35,12 @@ def get_user_id(token: str) -> str:
         user_id: str = payload.get("sub")
         if user_id is None:
             raise credentials_exception
+        
+        user = users_collection.find_one({"_id": ObjectId(user_id)})
+        if user is None:
+            raise credentials_exception
+        
     except JWTError:
         raise credentials_exception
     
-    return user_id
+    return CurrentUser(uid=user_id, profile=user["profile"], rooms=user["rooms"])
