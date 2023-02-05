e4f5df688dd42942f7f780a776471b7c4ee001c8
Youssef Attai <youssefgalalnazem@gmail.com>
2023-02-05 21:48:53 +0200

    Update room creation
    - Make sure room members' IDs are valid
    - Add room to all room members' room object

--- backend/router/room.py
+++ backend/router/room.py
@@ -1,7 +1,11 @@
-from fastapi import APIRouter, Depends
-from database import get_rooms_collection
+from bson import ObjectId
+import bson
+from fastapi import APIRouter, Depends, HTTPException, status
+from auth.jwt_utils import get_current_user
 from pymongo.collection import Collection
+from database.collections import get_rooms_collection, get_users_collection
 from schema.room import CreateRoomSchema
+from schema.user import CurrentUser
 
 
 router = APIRouter(prefix="/room")
@@ -10,7 +14,28 @@ router = APIRouter(prefix="/room")
 @router.post("/")
 async def create_room(
     room: CreateRoomSchema,
-    rooms_collection: Collection = Depends(get_rooms_collection)
+    current_user: CurrentUser = Depends(get_current_user),
+    rooms_collection: Collection = Depends(get_rooms_collection),
+    users_collection: Collection = Depends(get_users_collection)
 ):
+    for uid in room.users:
+        try:
+            if users_collection.count_documents({"_id": ObjectId(uid)}) != 1:
+                raise HTTPException(
+                    status.HTTP_400_BAD_REQUEST,
+                    f"User {uid} does not exist"
+                )
+        except bson.errors.InvalidId:
+            raise HTTPException(
+                status.HTTP_400_BAD_REQUEST,
+                f'Invalid ID: {uid}'
+            )
+
+    room.users.append(current_user.uid)
     result = rooms_collection.insert_one(room.dict())
-    return str(result.inserted_id)
+    users_collection.update_many({"_id": {"$in": [ObjectId(i) for i in room.users]}}, {
+        "$set": {
+            f"rooms.{result.inserted_id}": {}
+        }
+    })
+    return {"message": f"Created room {result.inserted_id}"}
