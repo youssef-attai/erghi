ee27d3aba4a76ad2395fa587fd57f04a00d1149c
Youssef Attai <youssefgalalnazem@gmail.com>
2023-02-05 05:53:43 +0200

    Create users and rooms database collections

--- backend/database.py
+++ backend/database.py
@@ -5,3 +5,12 @@ from env import MONGODB_CONNECTION_URL
 client = MongoClient(MONGODB_CONNECTION_URL)
 db = client.get_database('test_database')
 
+
+def get_users_collection():
+    users_collection = db.get_collection("users")
+    return users_collection
+
+
+def get_rooms_collection():
+    rooms_collection = db.get_collection("rooms")
+    return rooms_collection
