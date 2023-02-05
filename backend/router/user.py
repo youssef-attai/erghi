06ce2246df7ce394c7bd1320352ee41ff429ff65
Youssef Attai <youssefgalalnazem@gmail.com>
2023-02-05 21:46:29 +0200

    Refactor

--- backend/router/user.py
+++ backend/router/user.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter, Depends
 from pymongo.collection import Collection
-
-from database import get_users_collection
+from database.collections import get_users_collection
 from model.profile import Profile
 from model.user import User
-from schema.user import CreateUserSchema
+from schema.user import CreateUser
 from utils import hash_password
 
 router = APIRouter(prefix="/user")
@@ -12,17 +11,17 @@ router = APIRouter(prefix="/user")
 
 @router.post("/")
 async def create_user(
-    user: CreateUserSchema,
+    user: CreateUser,
     users_collection: Collection = Depends(get_users_collection)
 ):
     new_user = User(
         profile=Profile(
             username=user.username,
-            bio=""
+            bio="Hey, there!"
         ),
         rooms=[],
         password=hash_password(user.password)
     )
 
     result = users_collection.insert_one(new_user.dict())
-    return str(result.inserted_id)
+    return {"message": f"Created user {result.inserted_id}"}
