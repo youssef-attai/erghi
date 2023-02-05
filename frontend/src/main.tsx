efac570e8b00d0432745b26ace4e84b3e5e629b5
Youssef Attai <youssefgalalnazem@gmail.com>
2023-02-05 21:52:24 +0200

    Create the BrowserRouter

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Root from './Root'
 import './index.css'
+import Login, {action as loginAction} from './routes/Login'
+import SignUp from './routes/SignUp'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "login",
+        element: <Login />,
+        action: loginAction
+      },
+      {
+        path: "signup",
+        element: <SignUp />
+      }
+      // TODO: Create and add the chatting route. Make it protected by redirecting to login if no user is stored in localStorage 
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
