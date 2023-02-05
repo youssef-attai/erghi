854dc79f052e2a67a696580b9e364213a5d48225
Youssef Attai <youssefgalalnazem@gmail.com>
2023-02-05 05:58:31 +0200

    Create a function that decodes JWT

--- backend/auth/jwt_utils.py
+++ backend/auth/jwt_utils.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
+from fastapi import HTTPException, status
 
 SECRET_KEY = "potato"
 ALGORITHM = "HS256"
@@ -13,3 +14,20 @@ def create_access_token(data: dict, expires_delta: timedelta):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
+
+def get_user_id(token: str) -> str:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("sub")
+        if user_id is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    
+    return user_id
