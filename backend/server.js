f0add5aef27b948781a5d8a131964737dd0ad25a
Youssef Attai <youssefgalalnazem@gmail.com>
2023-02-06 22:29:40 +0200

    Use the authentication router

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 import express from 'express'
 
+import authRouter from './routers/auth.js';
+
+import { PORT } from './env.js';
+import './database.js';
+
 const app = express()
 
-app.listen(() => {
-    console.log("Running");
+app.use('/auth', authRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://127.0.0.1:${PORT}`);
 })
