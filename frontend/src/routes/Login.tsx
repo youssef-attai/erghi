a71fa640e330c9eb0ba95f848b8215fafbc74817
Youssef Attai <youssefgalalnazem@gmail.com>
2023-02-08 01:09:45 +0200

    Recreate the login route page

--- frontend/src/routes/Login.tsx
+++ frontend/src/routes/Login.tsx
@@ -1,47 +1,33 @@
-import { Form, Link, redirect, useNavigation } from 'react-router-dom'
-import { LoginProps } from '../contexts/AuthContext'
-
-
-type ActionProps = {
-    request: Request
-}
-
-// TIL: Apparently you can not use react hooks here in action handlers...
-export const action = ({ login }: LoginProps) => async ({ request }: ActionProps) => {
-    // TODO: Handle errors
-
-    const formData = await request.formData()
-    
-    const username: string = formData.get('username')?.toString()!
-    const password: string = formData.get('password')?.toString()!
-
-    await login(username, password)
-
-    return redirect("/chat")
-}
+import { useRef } from 'react'
+import { Link, Navigate, useNavigation } from 'react-router-dom'
+import { useAuth } from '../contexts/AuthContext'
 
 const Login = () => {
-    // TODO: useAuth, if already authenticated then redirect to /chat
-        // TODO: In /chat, useAuth, if not authenticated redirect to here, /login
+    const { login, currentUser } = useAuth()
+    const naviagtion = useNavigation()
 
-    // TODO: Display some kind of loading what navigation.state === submitting
-    // const navigation = useNavigation()
+    const usernameRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
 
+    console.log(naviagtion.state);
+
+    if (!currentUser)
     return (
         // navigation.state !== 'submitting' &&
-        <div className="container">
-            <Form className="card" method='post'>
-                <input type="text" name="username" id="username" placeholder='username' />
-                <input type="password" name="password" id="password" placeholder='password' />
-                <button type="submit">
-                    login
-                </button>
-                <Link to={"/signup"}>
-                    don't have an account? click here to create one
-                </Link>
-            </Form>
+        <div className="card" >
+            <input type="text" ref={usernameRef} name="username" id="username" placeholder='username' />
+            <input type="password" ref={passwordRef} name="password" id="password" placeholder='password' />
+            <button type="button" onClick={async () => {
+                await login(usernameRef.current!.value, passwordRef.current!.value)
+            }}>
+                login
+            </button>
+            <Link to={"/signup"}>
+                click here to create an account
+            </Link>
         </div>
     )
+    else return <Navigate to={'/chat'}/>
 }
 
 export default Login
