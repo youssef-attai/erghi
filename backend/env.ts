19d151e2f0e5a61416f31ff78f642076cc2d9842
Youssef Attai <youssefgalalnazem@gmail.com>
2023-02-07 03:00:04 +0200

    Read JWT environment variables

--- backend/env.ts
+++ backend/env.ts
@@ -1,6 +1,10 @@
-import * as dotenv from 'dotenv';
+import * as dotenv from 'dotenv'
 
 dotenv.config()
 
-export const MONGODB_CONNECTION_URL = process.env.MONGODB_CONNECTION_URL;
-export const PORT = process.env.PORT || 3030;
+export const MONGODB_CONNECTION_URL = process.env.MONGODB_CONNECTION_URL
+export const PORT = process.env.PORT || 3030
+export const ACCESS_TOKEN_EXPIRE_SECONDS = parseInt(process.env.ACCESS_TOKEN_EXPIRE_SECONDS!)
+export const ACCESS_TOKEN_SECRET_KEY = process.env.ACCESS_TOKEN_SECRET_KEY
+export const REFRESH_TOKEN_EXPIRE_SECONDS = parseInt(process.env.REFRESH_TOKEN_EXPIRE_SECONDS!)
+export const REFRESH_TOKEN_SECRET_KEY = process.env.REFRESH_TOKEN_SECRET_KEY
