e25fe67190536425f79942d99d1d55cba2a295e7
Youssef Attai <youssefgalalnazem@gmail.com>
2023-02-08 00:52:18 +0200

    Fix problem with CORS origin and credentials

--- backend/server.ts
+++ backend/server.ts
@@ -10,8 +10,12 @@ import './database/database.js'
 const app: Express = express()
 
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({
+    origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
+    credentials: true
+}))
 app.use(express.json())
+
 app.use('/auth', authRouter)
 
 app.listen(PORT, () => {
