1560e21ed56e2ebdcd3e1a147450fbafc463001b
Youssef Attai <youssefgalalnazem@gmail.com>
2023-02-08 01:09:57 +0200

    Recreate the sign up route page

--- frontend/src/routes/SignUp.tsx
+++ frontend/src/routes/SignUp.tsx
@@ -1,24 +1,27 @@
-import { Link } from 'react-router-dom'
-import { Form } from 'react-router-dom'
+import { Link, Navigate } from 'react-router-dom'
+import { useAuth } from '../contexts/AuthContext'
 
-// TODO: Create the signup action handler
 
 const SignUp = () => {
+    const { signUp, currentUser } = useAuth()
+
+    if (!currentUser)
     return (
-        <div className="container">
-            <Form className="card" method='post'>
-                <input type="text" name="username" id="username" placeholder='username' />
-                <input type="password" name="password" id="password" placeholder='password' />
-                <input type="password" name="confirmPassword" id="confirmPassword" placeholder='confirm password' />
-                <button type="submit">
-                    create account
-                </button>
-                <Link to={"/login"}>
-                    already have an account? click here to login
-                </Link>
-            </Form>
+        <div className="card" >
+            <input type="text" name="username" id="username" placeholder='username' />
+            <input type="password" name="password" id="password" placeholder='password' />
+            <input type="password" name="confirmPassword" id="confirmPassword" placeholder='confirm password' />
+            <button type="button" onClick={async () => {
+                await signUp("youssef", "123", "123")
+            }}>
+                create account
+            </button>
+            <Link to={"/login"}>
+                already have an account? click here to login
+            </Link>
         </div>
     )
+    else return <Navigate to={'/chat'}/>
 }
 
 export default SignUp
