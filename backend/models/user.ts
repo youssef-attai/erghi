fe67a4162a2e4e1f8ec9a6c7104b66f91a530a5f
Youssef Attai <youssefgalalnazem@gmail.com>
2023-02-07 03:03:13 +0200

    Create an initial user model

--- backend/models/user.ts
+++ backend/models/user.ts
@@ -1,5 +1,33 @@
 import mongoose from "mongoose";
 
 export default mongoose.model('User', new mongoose.Schema({
-    
+    profile: {
+        type: {
+            username: String,
+            bio: String,
+            _id: false
+        },
+        required: true
+    },
+    rooms: {
+        type: [{
+            rid: {
+                type: mongoose.Types.ObjectId,
+                ref: 'Room'
+            },
+            profile: {
+                username: String,
+                bio: String
+            }
+        }],
+        default: []
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    refresh: {
+        type: String,
+        required: true
+    }
 }));
