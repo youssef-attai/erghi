7d8278ca13c67d9830b27368f38669e9d5f620c1
Youssef Attai <youssefgalalnazem@gmail.com>
2023-02-07 04:51:34 +0200

    Move browser router into a separate component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,32 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Root from './Root'
-import './index.css'
-import Login, {action as loginAction} from './routes/Login'
-import SignUp from './routes/SignUp'
+import App from './App'
+import AuthProvider from './contexts/AuthContext'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        path: "login",
-        element: <Login />,
-        action: loginAction
-      },
-      {
-        path: "signup",
-        element: <SignUp />
-      }
-      // TODO: Create and add the chatting route. Make it protected by redirecting to login if no user is stored in localStorage 
-    ]
-  }
-])
+import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
 )
