810eb14e0af9049b3d072a23ab2bbfbf933580f5
Youssef Attai <youssefgalalnazem@gmail.com>
2023-02-07 03:01:23 +0200

    Fix CORS issues

--- backend/server.ts
+++ backend/server.ts
@@ -7,7 +7,7 @@ import './database/database.js';
 
 const app: Express = express()
 
-app.use('/auth', authRouter);
+app.use(cors())
 
 app.listen(PORT, () => {
     console.log(`Server running on http://127.0.0.1:${PORT}`);
