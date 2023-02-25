ec1a3b2a7393ef18d30cb43929c883df4882f54a
youssefattai <youssefgalalnazem@gmail.com>
2023-02-25 23:57:54 +0200

    Setup basic socket.io server

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,21 @@ import express from "express";
 import sessions from 'express-session';
 import connectDB from "./database.js";
 import authRouter from "./routers/auth.js";
-import { PORT, SESSION_SECRET } from './env.js';
+import { CLIENT_ADDRESS, PORT, SESSION_SECRET } from './env.js';
+import http from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+
 
 connectDB();
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: CLIENT_ADDRESS
+    }
+});
 
 app.use(sessions({
     secret: SESSION_SECRET,
@@ -24,6 +34,20 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/auth', authRouter);
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+    console.log('A user connected: ', socket.id);
+
+    socket.on('message', (data) => {
+        console.log('Received message:', data);
+        io.emit('message', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected: ', socket.id);
+    });
+});
+
+
+server.listen(PORT, () => {
     console.log(`Server started: http://localhost:${PORT}`);
 });
