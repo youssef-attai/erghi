a6ab9f723151b4a561e1b9a947185e6f7066eb9b
youssefattai <youssefgalalnazem@gmail.com>
2023-02-26 00:04:25 +0200

    Automatically login on page refresh

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import { useAsync } from "@youssef-attai/useful-hooks";
+import { useEffect } from "react";
 import Chat from "./components/Chat"
 import Login from "./components/Login";
 import { useAuth } from "./contexts/AuthContext"
+
 function App() {
   const { user, getCurrentUser } = useAuth();
+  const { execute: execGetCurrentUser, status, error } = useAsync(getCurrentUser);
+
+  useEffect(() => {
+    execGetCurrentUser();
+  }, []);
+
   return (
     <>
       {user ? (<Chat />) : (<Login />)}
