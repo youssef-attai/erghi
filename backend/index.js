888d894676c00c8854b141b39f622bea445cc417
youssefattai <youssefgalalnazem@gmail.com>
2023-02-25 15:58:57 +0200

    User express sessions middleware

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,20 @@
 import express from "express";
 import sessions from 'express-session';
 import connectDB from "./database.js";
+import { SESSION_SECRET } from './env.js';
 
 connectDB();
 
 const app = express();
+
+app.use(sessions({
+    secret: SESSION_SECRET,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+    },
+    resave: true,
+    saveUninitialized: false,
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
