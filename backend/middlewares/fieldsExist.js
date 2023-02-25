9f38af150471c0e689c776556c87ab62ab3a2e73
youssefattai <youssefgalalnazem@gmail.com>
2023-02-26 00:01:38 +0200

    Refactor

--- backend/middlewares/fieldsExist.js
+++ backend/middlewares/fieldsExist.js
@@ -2,7 +2,7 @@ export default function (...fields) {
     return function middleware(req, res, next) {
         for (const field of fields) {
             if (!req.body[field]) {
-                res.send(`Missing ${field}`);
+                res.status(400).json({ message: `missing ${field}` });
                 return;
             }
         }
