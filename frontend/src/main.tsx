bb1c16be87ddfba77784433ad14d3081430f6155
youssefattai <youssefgalalnazem@gmail.com>
2023-02-26 00:05:27 +0200

    Use auth provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { AuthProvider } from './contexts/AuthContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  // </React.StrictMode>
 )
