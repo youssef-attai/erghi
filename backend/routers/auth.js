3a81be4b7c5a9acdd6d507ea200aa5689a7e218e
youssefattai <youssefgalalnazem@gmail.com>
2023-02-25 16:04:44 +0200

    Create the authentication router

---
+++ backend/routers/auth.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { createAccount, login, logout } from "../controllers/auth.js";
+import { ensureAuthenticated, ensureFieldsExist } from "../middlewares.js";
+
+const router = Router();
+
+router.post('/create', ensureFieldsExist('username', 'password'), createAccount);
+
+router.post('/login', ensureFieldsExist('username', 'password'), login);
+
+router.get('/logout', ensureAuthenticated, logout);
+
+export default router;
