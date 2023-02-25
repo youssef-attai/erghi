74d0089dd672ed4a87fd361b833cd41571f9adc9
youssefattai <youssefgalalnazem@gmail.com>
2023-02-25 15:59:39 +0200

    Create middleware for validating req.body fields

---
+++ backend/middlewares.js
@@ -0,0 +1,12 @@
+export function ensureFieldsExist(...fields) {
+    return function middleware(req, res, next) {
+        for (const field of fields) {
+            if (!req.body[field]) {
+                res.send(`Missing ${field}`);
+                return;
+            }
+        }
+
+        next();
+    }
+}
