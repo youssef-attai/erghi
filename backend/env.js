56149b1a6d3c7b5f399af7c5d1b03ff11898c78a
youssefattai <youssefgalalnazem@gmail.com>
2023-02-25 23:56:39 +0200

    Load React client address from env file

--- backend/env.js
+++ backend/env.js
@@ -5,3 +5,4 @@ dotenv.config();
 export const PORT = process.env.PORT;
 export const MONGO_URI = process.env.MONGO_URI;
 export const SESSION_SECRET = process.env.SESSION_SECRET;
+export const CLIENT_ADDRESS = process.env.CLIENT_ADDRESS;
