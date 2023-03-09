34b861a3555add118f5d30fd456869fb2ff426ba
youssef-attai <youssefgalalnazem@gmail.com>
2023-03-10 00:17:06 +0200

    Use styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import { useEffect } from "react";
 import Chat from "./components/Chat"
 import Login from "./components/Login";
 import { useAuth } from "./contexts/AuthContext"
+import colors from "./styles/colors";
+
 
 function App() {
   const { user, me } = useAuth();
@@ -13,10 +15,55 @@ function App() {
   }, []);
 
   return (
-    <>
-      {user ? (<Chat />) : (<Login />)}
-    </>
+    <div style={styles.container}>
+      <div style={styles.navbar}>
+        <img src="erghi.svg" alt="Logo" style={styles.logo} />
+        <img src="github.svg" alt="GitHub" style={styles.github} />
+      </div>
+      <div style={styles.page}>
+        {user ? (<Chat />) : (<Login />)}
+      </div>
+    </div>
   )
 }
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column' as const,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+    width: '100vw',
+    backgroundColor: colors.darkPrimary,
+  },
+  navbar: {
+    display: 'flex',
+    flexDirection: 'row' as const,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    height: 60,
+    width: '100%',
+    padding: '0 20px',
+    backgroundColor: colors.darkPrimary,
+  },
+  page: {
+    display: 'flex',
+    flexDirection: 'column' as const,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%',
+    width: '100%',
+    backgroundColor: colors.darkPrimary,
+  },
+  logo: {
+    height: 40,
+    userSelect: 'none' as const,
+  },
+  github: {
+    height: 40,
+    userSelect: 'none' as const,
+  },
+};
 
 export default App
--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,36 +1,64 @@
 import { useRef } from 'react';
 import { useAuth } from '../contexts/AuthContext';
 import { useAsync } from '@youssef-attai/useful-hooks';
+import colors from '../styles/colors';
 
 function Login() {
-    const { login } = useAuth();
+  const { login } = useAuth();
 
-    const usernameRef = useRef<HTMLInputElement>(null);
-    const passwordRef = useRef<HTMLInputElement>(null);
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
 
-    const { execute: execLogin, status, error } = useAsync<void, { message: string }>(login);
+  const { execute: execLogin, status, error } = useAsync<void, { message: string }>(login);
 
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
-        const username = usernameRef.current?.value;
-        const password = passwordRef.current?.value;
-        try {
-            if (username && password) await execLogin(username, password);
-        } catch (error) {
-            console.error(error);
-        }
-    };
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const username = usernameRef.current?.value;
+    const password = passwordRef.current?.value;
+    try {
+      if (username && password) await execLogin(username, password);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    return (
-        <>
-            {<p>{error?.message}</p>}
-            <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="username" ref={usernameRef} />
-                <input type="password" placeholder="password" ref={passwordRef} />
-                <button type="submit">Login</button>
-            </form>
-        </>
-    );
+  return (
+    <div >
+      {<p>{error?.message}</p>}
+      <form onSubmit={handleSubmit} style={styles.form}>
+        <input type="text" placeholder="username" ref={usernameRef} style={styles.input} />
+        <input type="password" placeholder="password" ref={passwordRef} style={styles.input} />
+        <button type="submit" style={styles.button} >Login</button>
+      </form>
+    </div>
+  );
+}
+
+const styles = {
+  form: {
+    display: 'flex',
+    flexDirection: 'column' as const,
+    alignItems: 'center',
+    gap: 10,
+  },
+  input: {
+    padding: 10,
+    borderRadius: 20,
+    border: 'none',
+    outline: 'none',
+    backgroundColor: colors.darkSecondary,
+    color: colors.secondary
+  },
+  button: {
+    padding: 10,
+    borderRadius: 20,
+    border: 'none',
+    outline: 'none',
+    backgroundColor: colors.primary,
+    color: colors.text,
+    width: 100,
+    cursor: 'pointer',
+  }
 }
 
 export default Login;
