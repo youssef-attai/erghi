fc1a3600350d0861ce24ff6e8f27fbb2412b45c9
youssef-attai <youssefgalalnazem@gmail.com>
2023-03-10 18:41:08 +0200

    Create initial room model

---
+++ backend/models/Room.js
@@ -0,0 +1,39 @@
+
+import mongoose from 'mongoose';
+
+const roomSchema = new mongoose.Schema({
+    roomName: {
+        type: String,
+        required: true,
+    },
+    messages: {
+        type: [{
+            sender: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User',
+            },
+            content: {
+                type: String,
+                required: true,
+            },
+            timestamp: {
+                type: Date,
+                required: true,
+            },
+        }],
+        required: true,
+        default: [],
+    },
+    members: {
+        type: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        }],
+        required: true,
+        default: [],
+    },
+});
+
+const Room = mongoose.model('Room', roomSchema);
+
+export default Room;
