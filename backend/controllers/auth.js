95b12a33672209d7983fc98c9ee44227695598d3
youssef-attai <youssefgalalnazem@gmail.com>
2023-03-11 09:52:51 +0200

    Return user ID on login

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,45 +1,50 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
+
 export async function createAccount(req, res) {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    try {
-        const newUser = new User({ username, password: hashedPassword });
-        await newUser.save();
+  try {
+    const newUser = new User({ username, password: hashedPassword });
+    await newUser.save();
 
-        req.session.userId = newUser._id.toString();
-        res.send(`welcome, ${newUser.username}`);
-    } catch (error) {
-        res.status(409).send('username already exists');
-    }
+    req.session.userId = newUser._id.toString();
+
+    // TODO: Return proper response
+    res.send(`welcome, ${newUser.username}`);
+  } catch (error) {
+    // TODO: Return proper response
+    res.status(409).send("username already exists");
+  }
 }
 
 export async function login(req, res) {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
-    const foundUser = await User.findOne({ username });
-    if (!foundUser) return res.status(400).json({ message: 'invalid username' });
+  const foundUser = await User.findOne({ username });
+  if (!foundUser) return res.status(400).json({ message: "invalid username" });
 
-    const passwordMatch = await bcrypt.compare(password, foundUser.password);
-    if (!passwordMatch) return res.status(400).json({ message: 'invalid password' });
+  const passwordMatch = await bcrypt.compare(password, foundUser.password);
+  if (!passwordMatch)
+    return res.status(400).json({ message: "invalid password" });
 
-    req.session.userId = foundUser._id.toString();
-    
-    res.json({ username: foundUser.username });
+  req.session.userId = foundUser._id.toString();
+
+  res.json({ username: foundUser.username, id: req.session.userId });
 }
 
 export function logout(req, res) {
-    const _id = req.session.userId;
-    req.session.destroy(() => {
-        console.log('session destroyed');
-    });
-    res.json({ message: `user ${_id} logged out` });
+  const _id = req.session.userId;
+  req.session.destroy(() => {
+    console.log("session destroyed");
+  });
+  res.json({ message: `user ${_id} logged out` });
 }
 
 export async function me(req, res) {
-    const _id = req.session.userId;
-    const { username } = await User.findOne({ _id });
-    res.json({ username });
+  const _id = req.session.userId;
+  const { username } = await User.findOne({ _id });
+  res.json({ username, id: _id });
 }
