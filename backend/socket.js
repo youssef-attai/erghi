b6a848e6a7af29bc5383189d975a14dab5f479d8
youssef-attai <youssefgalalnazem@gmail.com>
2023-03-11 09:00:08 +0200

    Join rooms on socket connection

--- backend/socket.js
+++ backend/socket.js
@@ -1,17 +1,25 @@
+import Room from './models/Room.js';
+
 export default function setupSocketIO(io) {
-    io.on('connection', (socket) => {
+    io.on('connection', async (socket) => {
         const username = socket.handshake.query.username;
+        const rooms = await Room.find({ users: socket.handshake.query.userId }, { _id: 1 }); 
 
         console.log('[CONNECTED]   ', username, socket.id);
 
+        rooms.forEach(room => {
+            socket.join(room._id.toString());
+        });
+
         socket.on('message', (data) => {
             console.log(username, socket.id, ':', data);
-            io.emit('message', data);
+
+            const room = data.room;
+            io.to(room).emit('message', data);
         });
 
         socket.on('disconnect', () => {
             console.log('[DISCONNECTED]', username, socket.id);
         });
     });
-
 }
